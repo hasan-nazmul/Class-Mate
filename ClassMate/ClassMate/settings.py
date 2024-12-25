e80955fccb0a943cc63dd0f3c4da59bb80f5d32f
hasan-nazmul <nh2826239@gmail.com>
2024-12-26 03:36:33 +0600

    adjusted js files

--- ClassMate/ClassMate/settings.py
+++ ClassMate/ClassMate/settings.py
@@ -129,7 +129,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'public/static'),
 ]
 
---
+++ ClassMate/public/static/js/app.js
@@ -0,0 +1,26 @@
+function toggleSidebar() {
+    console.log('toggling')
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-250px';
+    } else {
+        sidebar.style.left = '0px';
+    }
+}
+
+function toggleNestedMenu(menuId, element) {
+    var nestedMenu = document.getElementById(menuId);
+    var icon = element.querySelector('i');
+    if (nestedMenu.style.display === 'block') {
+        nestedMenu.style.display = 'none';
+        icon.classList.replace('fa-chevron-up', 'fa-chevron-down');
+    } else {
+        nestedMenu.style.display = 'block';
+        icon.classList.replace('fa-chevron-down', 'fa-chevron-up');
+    }
+}
+
+function openClassMenuModal() {
+    $('#classMenuModal').modal('show');
+    console.log('clicked');
+}
